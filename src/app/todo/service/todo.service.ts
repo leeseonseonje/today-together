640f2e00c241de291c188dbd6e1f6a3c12271476
seonje <leeseonje9323@gmail.com>
2023-04-07 01:08:29 +0900

    트랜잭션 적용

--- src/app/todo/service/todo.service.ts
+++ src/app/todo/service/todo.service.ts
@@ -3,39 +3,38 @@ import {Todo} from '../domain/todo.entity';
 import {TodoStatus} from '../domain/todo-status.enum';
 import {LocalDate} from 'js-joda';
 import {Injectable} from '@nestjs/common';
-import {getConnection} from 'typeorm';
+import {getConnection, getManager} from 'typeorm';
 
 @Injectable()
 export class TodoService {
 
-  private todoRepository: TodoRepository;
-  constructor(
-  ) {
-  }
-
   async save(memberId: number, text: string) {
-    this.todoRepository = getConnection().getCustomRepository(TodoRepository);
+    const todoRepository = getConnection().getCustomRepository(TodoRepository);
     const todo = new Todo(memberId, text, LocalDate.now(), TodoStatus.INCOMPLETE);
-    return await this.todoRepository.save(todo);
+    return await todoRepository.save(todo);
   }
 
   async updateText(id: number, text: string) {
-    await this.todoRepository.updateText(id, text);
+    const todoRepository = getConnection().getCustomRepository(TodoRepository);
+    await todoRepository.updateText(id, text);
   }
 
   async complete(id: number) {
-    await this.todoRepository.complete(id);
+    const todoRepository = getConnection().getCustomRepository(TodoRepository);
+    await todoRepository.complete(id);
   }
 
   async getTodayTodo(memberId: number) {
-    this.todoRepository = getConnection().getCustomRepository(TodoRepository);
-    const incompleteTodos = await this.todoRepository.findIncompleteTodos(memberId);
+    return await getManager().transaction(async manager => {
+      const todoRepository = manager.getCustomRepository(TodoRepository);
 
-    for (const todo of incompleteTodos) {
-      const newTodayTodo = new Todo(memberId, todo.text, LocalDate.now(), TodoStatus.INCOMPLETE);
-      await this.todoRepository.save(newTodayTodo);
-    }
+      const incompleteTodos = await todoRepository.findIncompleteTodos(memberId);
 
-    return this.todoRepository.findTodayTodos(memberId);
+      for (const todo of incompleteTodos) {
+        const newTodayTodo = new Todo(memberId, todo.text, LocalDate.now(), TodoStatus.INCOMPLETE);
+        await todoRepository.save(newTodayTodo);
+      }
+      return await todoRepository.findTodayTodos(memberId);
+    });
   }
 }
