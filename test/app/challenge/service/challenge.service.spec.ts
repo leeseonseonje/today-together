678ec1348ccafa03dae34b18e62463f2111bab6a
seonje <leeseonje9323@gmail.com>
2023-04-12 23:06:34 +0900

    entity 설계

---
+++ test/app/challenge/service/challenge.service.spec.ts
@@ -0,0 +1,48 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import {getConnection} from 'typeorm';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {dbConfig} from '../../../../src/db/config';
+import {ChallengeService} from '../../../../src/app/challenge/service/challenge.service';
+import {ChallengeModule} from '../../../../src/app/challenge/challenge.module';
+import {Challenge} from '../../../../src/app/challenge/challenge.entity';
+import {LocalDate, LocalDateTime} from 'js-joda';
+
+describe('challenge Service Integration Test', () => {
+
+  let sut: ChallengeService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(dbConfig), ChallengeModule],
+    }).compile();
+
+    sut = module.get<ChallengeService>(ChallengeService);
+  });
+
+  afterEach(async () => {
+    await getConnection().dropDatabase();
+    await getConnection().close();
+  });
+
+  it('하루에 commit을 몇 번 한지 조회 (횟수만)', async () => {
+    const repository = getConnection().getRepository(Challenge);
+    await repository.save(new Challenge(2, 1, 100, LocalDateTime.now()));
+    await repository.save(new Challenge(2, 2, 50, LocalDateTime.now()));
+    await repository.save(new Challenge(2, 1, 30, LocalDateTime.now().plusDays(1)));
+
+    const result = await sut.getDayCommit(1, LocalDate.now());
+
+    expect(result.commits).toBe(100);
+  });
+
+  it('모든날의 commit 횟수 조회 (한달 단위)', async () => {
+    const repository = getConnection().getRepository(Challenge);
+    await repository.save(new Challenge(2, 1, 100, LocalDateTime.now()));
+    await repository.save(new Challenge(2, 1, 50, LocalDateTime.now().plusMonths(1)));
+
+    const result = await sut.getMonthCommit(1, LocalDate.now());
+
+    expect(result.length).toBe(1);
+    expect(result[0].commits).toBe(100);
+  });
+});
--- test/app/todo/service/todo.service.spec.ts
+++ test/app/todo/service/todo.service.spec.ts
@@ -6,8 +6,10 @@ import {TodoModule} from '../../../../src/app/todo/todo.module';
 import {TodoService} from '../../../../src/app/todo/service/todo.service';
 import {Todo} from '../../../../src/app/todo/domain/todo.entity';
 import {TodoStatus} from '../../../../src/app/todo/domain/todo-status.enum';
-import {LocalDate} from 'js-joda';
+import {LocalDate, LocalDateTime} from 'js-joda';
 import {TodoRepository} from '../../../../src/app/todo/repository/todo.repository';
+import {Challenge} from '../../../../src/app/challenge/challenge.entity';
+import {ChallengeRepository} from '../../../../src/app/challenge/repository/challenge.repository';
 
 describe('todo Service Integration Test', () => {
 
@@ -22,9 +24,40 @@ describe('todo Service Integration Test', () => {
   });
 
   afterEach(async () => {
-    await getConnection().query('delete from todo');
+    await getConnection().dropDatabase();
     await getConnection().close();
   })
+
+  it('complete 상태값 COMPLETE로 변경, commit, 그 날 최초 커밋 시 challenge save', async () => {
+    const todo = await getConnection().getCustomRepository(TodoRepository)
+      .save(new Todo(1, 'text1', LocalDate.now(), TodoStatus.INCOMPLETE));
+
+    const result = await sut.complete(todo.id, 1) as Challenge;
+
+    const findTodo = await getConnection().getCustomRepository(TodoRepository).findOne({
+      where: {
+        id: todo.id,
+      }
+    });
+    expect(findTodo.status).toBe(TodoStatus.COMPLETE);
+    expect(result.commits).toBe(1);
+  });
+
+  it('최초 커밋 아닐 시 challenge update (commits++)', async () => {
+    const todo = await getConnection().getCustomRepository(TodoRepository)
+      .save(new Todo(1, 'text1', LocalDate.now(), TodoStatus.INCOMPLETE));
+    const challenge = await getConnection().getCustomRepository(ChallengeRepository)
+      .save(new Challenge(todo.id, 1, 1, LocalDateTime.now()));
+
+    const result = await sut.complete(todo.id, 1);
+
+    const findChallenge = await getConnection().getCustomRepository(ChallengeRepository).findOne({
+      where: {
+        id: challenge.id,
+      }
+    });
+    expect(findChallenge.commits).toBe(2);
+  });
 
   it('오늘 할 일 목록 호출 미완료한 일정이 있으면 오늘 일정으로 자동 등록', async () => {
     await getConnection().getCustomRepository(TodoRepository).save(new Todo(1, 'text1', LocalDate.of(1, 8, 2), TodoStatus.INCOMPLETE));
