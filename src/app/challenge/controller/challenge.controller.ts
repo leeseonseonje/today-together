792c1df39a387eac671ec9c91c373f6a73a4a473
seonje <leeseonje9323@gmail.com>
2023-04-16 00:11:22 +0900

    challenge controller 생성

--- src/app/challenge/controller/challenge.controller.ts
+++ src/app/challenge/controller/challenge.controller.ts
@@ -1,8 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, Query} from '@nestjs/common';
 import { ChallengeService } from '../service/challenge.service';
+import {DateTimeUtil} from '../../../util/date-time.util';
 
-@Controller('challenge')
+@Controller('challenges')
 export class ChallengeController {
-  constructor(private readonly challengeService: ChallengeService) {}
+  constructor(private readonly challengeService: ChallengeService) {
+  }
 
+  @Get('/day/:memberId')
+  async getDayCommit(@Param() memberId: number,
+                     @Query() day: string) {
+    return await this.challengeService.getDayCommit(memberId, DateTimeUtil.toLocalDate(day));
+  }
+
+  @Get('/month/:memberId')
+  async getMonthCommit(@Param() memberId: number,
+                     @Query() day: string) {
+    return await this.challengeService.getMonthCommit(memberId, DateTimeUtil.toLocalDate(day));
+  }
+
+  @Get('/history/:memberId')
+  async dayCommitHistory(@Param() memberId: number,
+                       @Query() day: string) {
+    return await this.challengeService.dayCommitHistory(memberId, DateTimeUtil.toLocalDate(day));
+  }
 }
